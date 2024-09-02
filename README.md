892abb0867c3ab9edb01437ea92e5aa9b433328c
Dym Sohin <you@example.com>
2024-09-02 13:21:52 +0200

    added permissions

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # blog
 
-> following
+excersize in creating laravel-based web-app, following the tutorials:
+
+1. Part 1: https://www.youtube.com/watch?v=iVThaG_sAt0
+2. Part 2: https://www.youtube.com/watch?v=2ZkePm2D6SI
+3. Part 3: https://www.youtube.com/watch?v=lCHG7wqrXrk
 
 ## tech
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
@@ -8,24 +7,6 @@
     <meta name="author" content="Dym">
     <meta name="description" content="{{ $metaDescription }}">
 
-    <style>
-        @import url('https://fonts.googleapis.com/css?family=Karla:400,700&display=swap');
-
-        .font-family-karla {
-            font-family: karla;
-        }
-
-        pre {
-            padding:1rem;
-            word-wrap: break-word;
-            background: #222;
-            color: #ccc;
-            border-radius: 0.5rem;
-            margin-bottom: 1rem;
-            max-width: 40rem;
-            overflow: scroll;
-        }
-    </style>
 
     <!-- Font Awesome -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js" integrity="sha256-KzZiKy0DWYsnwMF+X1DvQngQ2/FxF7MF3Ff72XcpuPs=" crossorigin="anonymous"></script>
@@ -34,35 +15,6 @@
 
 </head>
 <body class="bg-gray-50 font-family-karla">
-
-    <!-- Top Bar Nav -->
-    <nav class="w-full py-4 bg-blue-800 shadow">
-        <div class="w-full container mx-auto flex flex-wrap items-center justify-between">
-
-            <nav>
-                <ul class="flex items-center justify-between font-bold text-sm text-white uppercase no-underline">
-                    <li><a class="hover:text-gray-200 hover:underline px-4" href="#">Shop</a></li>
-                    <li><a class="hover:text-gray-200 hover:underline px-4" href="#">About</a></li>
-                </ul>
-            </nav>
-
-            <div class="flex items-center text-lg no-underline text-white pr-6">
-                <a class="" href="#">
-                    <i class="fab fa-facebook"></i>
-                </a>
-                <a class="pl-6" href="#">
-                    <i class="fab fa-instagram"></i>
-                </a>
-                <a class="pl-6" href="#">
-                    <i class="fab fa-twitter"></i>
-                </a>
-                <a class="pl-6" href="#">
-                    <i class="fab fa-linkedin"></i>
-                </a>
-            </div>
-        </div>
-
-    </nav>
 
     <!-- Text Header -->
     <header class="w-full container mx-auto">
@@ -88,18 +40,60 @@
             </a>
         </div>
         <div :class="open ? 'block': 'hidden'" class="w-full flex-grow sm:flex sm:items-center sm:w-auto">
-            <div class="w-full container mx-auto flex flex-col sm:flex-row items-center justify-center text-sm font-bold uppercase mt-0 px-6 py-2">
-                <a href="{{ route('home') }}" class="hover:bg-blue-600 hover:text-white rounded py-2 px-4 mx-2">Home</a>
-                @foreach( $categories as $category )
-                    <a href="{{ route('by-category', $category) }}"
-                        class="hover:bg-blue-600 hover:text-white rounded py-2 px-4 mx-2
-                        {{ request('category')?->slug === $category?->slug
-                            ? 'bg-blue-600 text-white'
-                            : ''
-                        }}"
-                    >{{ $category->title }}</a>
-                @endforeach
-                <a href="{{ route('about-us') }}" class="hover:bg-blue-600 hover:text-white rounded py-2 px-4 mx-2">About Us</a>
+            <div class="w-full container mx-auto flex flex-row items-center justify-between text-sm font-bold uppercase mt-0 px-6 py-2">
+                <div>
+                    <a href="{{ route('home') }}" class="hover:bg-blue-600 hover:text-white rounded py-2 px-4 mx-2">Home</a>
+                    @foreach( $categories as $category )
+                        <a href="{{ route('by-category', $category) }}"
+                            class="hover:bg-blue-600 hover:text-white rounded py-2 px-4 mx-2
+                            {{ request('category')?->slug === $category?->slug
+                                ? 'bg-blue-600 text-white'
+                                : ''
+                            }}"
+                        >{{ $category->title }}</a>
+                    @endforeach
+                    <a href="{{ route('about-us') }}" class="hover:bg-blue-600 hover:text-white rounded py-2 px-4 mx-2">About Us</a>
+                </div>
+
+                <div class="flex items-center ms-6">
+                    @auth
+                        <x-dropdown align="right" width="48">
+                            <x-slot name="trigger">
+                                <button class="hover:bg-blue-600 hover:text-white rounded py-2 px-4 mx-2 flex flex-nowrap items-center">
+                                    <div>{{ Auth::user()->name }}</div>
+
+                                    <div class="ms-1">
+                                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                        </svg>
+                                    </div>
+                                </button>
+                            </x-slot>
+
+                            <x-slot name="content">
+                                <x-dropdown-link :href="route('profile.edit')">
+                                    {{ __('Profile') }}
+                                </x-dropdown-link>
+
+                                <!-- Authentication -->
+                                <form method="POST" action="{{ route('logout') }}">
+                                    @csrf
+
+                                    <x-dropdown-link :href="route('logout')"
+                                            onclick="event.preventDefault();
+                                                        this.closest('form').submit();">
+                                        {{ __('Log Out') }}
+                                    </x-dropdown-link>
+                                </form>
+                            </x-slot>
+                        </x-dropdown>
+                    @endauth
+                    @guest
+                        <a href="{{ route('login') }}" class="hover:bg-blue-600 hover:text-white rounded py-2 px-4 mx-2">Login</a>
+                        <a href="{{ route('register') }}" class="bg-blue-600 text-white rounded py-2 px-4 mx-2">Register</a>
+                    @endguest
+                </div>
+
             </div>
         </div>
     </nav>
